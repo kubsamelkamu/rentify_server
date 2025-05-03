c54a11cbeccb193d833e827a3e10e3e31e105345
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-04 00:12:48 +0300

    add lint script

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig({
+  files: ['**/*.{js,mjs,cjs}'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    globals: globals.node,
+  },
+  extends: [js.configs.recommended], 
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+  },
+});
