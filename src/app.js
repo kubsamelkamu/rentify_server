55d6de8ddfba9b6e69d15691b8567638189218c2
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-06 21:10:59 +0300

    import authRoute: & use it

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
+import authRoutes from './routes/auth.js';
 
 dotenv.config();
 const app = express();
@@ -10,7 +11,7 @@ export const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(json());
-
+app.use('/api/auth', authRoutes);
 app.get('/api/health', (req, res) => res.json({ status: 'OK' }));
 
 export default app;
