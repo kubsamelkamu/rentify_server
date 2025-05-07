f3858e63b8111e4685dbb876d2e55471a7b37af6
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-07 12:07:11 +0300

    corrected elistnt config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,26 @@ import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig({
-  files: ['**/*.{js,mjs,cjs}'],
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    globals: globals.node,
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node,
+    },
+    extends: [js.configs.recommended],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
   },
-  extends: [js.configs.recommended], 
-  rules: {
-    semi: ['error', 'always'],
-    quotes: ['error', 'single'],
+  {
+    files: ['client/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
   },
-});
+]);
