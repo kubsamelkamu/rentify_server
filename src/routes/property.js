f6ae2f717b1c7465778ab110cdc27b35c23b4c2f
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-11 23:44:42 +0300

    propertyRoute api end-point

--- src/routes/property.js
+++ src/routes/property.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import { createProperty, getAllProperties, getPropertyById, updateProperty, deleteProperty } from '../controllers/property.js';
+import { upload } from '../middlewares/upload.js';
+import { uploadPropertyImages } from '../controllers/property.js';
 
 const router = Router();
 
@@ -9,5 +11,6 @@ router.get('/', getAllProperties);
 router.get('/:id', getPropertyById);
 router.put('/:id', auth, updateProperty);
 router.delete('/:id', auth, deleteProperty);
+router.post('/:id/images', auth, upload.array('images', 2), uploadPropertyImages);
 
 export default router;
