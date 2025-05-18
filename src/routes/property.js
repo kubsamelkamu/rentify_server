10de1078209bd70b0871e9f673730e97a7c19aa7
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-18 18:43:03 +0300

    import & use message controller

--- src/routes/property.js
+++ src/routes/property.js
@@ -3,6 +3,7 @@ import auth from '../middlewares/auth.js';
 import { createProperty, getAllProperties, getPropertyById, updateProperty, deleteProperty } from '../controllers/property.js';
 import { upload } from '../middlewares/upload.js';
 import { uploadPropertyImages } from '../controllers/property.js';
+import { sendMessage } from '../controllers/message.js';
 
 const router = Router();
 
@@ -12,5 +13,7 @@ router.get('/:id', getPropertyById);
 router.put('/:id', auth, updateProperty);
 router.delete('/:id', auth, deleteProperty);
 router.post('/:id/images', auth, upload.array('images', 2), uploadPropertyImages);
+
+router.post('/:id/messages',auth, sendMessage);
 
 export default router;
