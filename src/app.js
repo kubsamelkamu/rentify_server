caa6246f88a217925911b2ac5b53d1dae6c03fe9
Kubsa Melkamu <kubsamlkm@gmail.com>
2025-05-25 23:52:28 +0300

    add booking route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 import authRoutes from './routes/auth.js';
+import bookingRoutes from './routes/booking.js';
 import propertyRoutes from './routes/property.js';
 
 dotenv.config();
@@ -13,6 +14,7 @@ export const prisma = new PrismaClient();
 app.use(cors());
 app.use(json());
 app.use('/api/auth', authRoutes);
+app.use('/api/bookings', bookingRoutes);
 app.use('/api/properties', propertyRoutes);
 app.get('/api/health', (req, res) => res.json({ status: 'OK' }));
 
